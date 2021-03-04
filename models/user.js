33dd8211fa2b883ceb4f7337df4ddb16ea4b651c
Basharath <basharath.in@gmail.com>
2021-03-04 23:17:26 +0530

    Adds user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+})
+
+const User = mongoose.model('User', userSchema);
+
+
+const validateUser = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string().email().required()
+  })
+  return schema.validate(schema);
+}
+
+module.exports = {
+  User,
+  validate: validateUser
+}
