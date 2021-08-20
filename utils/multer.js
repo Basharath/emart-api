f3e845ff276f8a4c2b6298eb140997e72b7cc26a
Basharath <basharath.in@gmail.com>
2021-08-20 15:52:47 +0530

    Updates callback to throw error on invalid file type

--- utils/multer.js
+++ utils/multer.js
@@ -10,7 +10,7 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, files, cb) => {
     const mime = MIME_TYPE[files.mimetype];
-    if (!mime) return cb(null, false);
+    if (!mime) return cb(new Error('File type is not supported'), false);
     cb(null, true);
   },
 });
