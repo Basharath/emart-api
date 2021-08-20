e24085ce048e497b28857d80f75f57cf9688dc22
Basharath <basharath.in@gmail.com>
2021-08-20 15:54:49 +0530

    Add adds max image upload limit

--- routes/products.js
+++ routes/products.js
@@ -13,8 +13,10 @@ const {
   rateProduct,
 } = require('../controllers/products');
 
+const LIMIT = process.env.productLimit || 4;
+
 router.get('/', getProducts);
-router.post('/', [upload.array('product'), auth, vendor], addProduct);
+router.post('/', [upload.array('product', LIMIT), auth, vendor], addProduct);
 router.put('/:id', [upload.array('product'), auth, vendor], updateProduct);
 router.get('/:id', getProduct);
 router.delete('/:id', [auth, vendor], deleteProduct);
