b1301bcc62a94190378310b434194082a41adbd8
Basharath <basharath.in@gmail.com>
2021-08-22 22:35:01 +0530

    Updates product population and cart updates

--- controllers/cart.js
+++ controllers/cart.js
@@ -4,11 +4,11 @@ const { Product } = require('../models/product');
 const getCart = async (req, res, next) => {
   const userId = req.user.id;
   try {
-    const cart = await Cart.findOne({ userId });
-    // const cart = await Cart.findOne({ userId }).populate({
-    //   path: 'items.productId',
-    //   select: 'title images',
-    // });
+    // const cart = await Cart.findOne({ userId });
+    const cart = await Cart.findOne({ userId }).populate({
+      path: 'items.product',
+      select: 'name images seller',
+    });
     if (!cart) return res.status(404).send('cart is empty');
     return res.send(cart);
   } catch (err) {
@@ -17,14 +17,17 @@ const getCart = async (req, res, next) => {
 };
 
 const updateCart = async (req, res, next) => {
-  const { productId, quantity } = req.body;
+  const { product: productId, quantity } = req.body;
   const userId = req.user.id;
 
-  const { error } = validate({ productId, quantity, userId });
+  const { error } = validate({ product: productId, quantity, userId });
   if (error) return res.status(400).send(error.details[0].message);
 
   try {
-    let cart = await Cart.findOne({ userId });
+    let cart = await Cart.findOne({ userId }).populate({
+      path: 'items.product',
+      select: 'name images',
+    });
     const product = await Product.findById(productId);
     if (!product) return res.status(500).send('Product not found');
 
@@ -33,36 +36,39 @@ const updateCart = async (req, res, next) => {
         userId,
         items: [
           {
-            productId,
+            product: productId,
             quantity,
-            price: product.price,
+            price: product.offer,
           },
         ],
       };
+      // cart = new Cart.populate(cartData, 'items.product');
       cart = new Cart(cartData);
 
-      const data = await cart.save();
+      cart = await cart.save();
+      const data = await cart.populate('items.product').execPopulate();
       return res.status(201).send(data);
     }
 
     const index = cart.items.findIndex(
-      (i) => i.productId.toString() === productId.toString()
+      (i) => i.product._id.toString() === productId.toString()
     );
 
     if (index !== -1 && quantity > 0) {
-      cart.items[index].quantity = quantity;
-      cart.items[index].price = product.price;
+      cart.items[index].quantity += quantity;
+      // cart.items[index].price = product.offer;
     } else if (index === -1 && quantity > 0) {
       cart.items.push({
-        productId,
+        product: productId,
         quantity,
-        price: product.price,
+        price: product.offer,
       });
     } else if (quantity === 0) {
       cart.items.splice(index, 1);
     } else return res.status(400).send('Invalid request');
 
-    const cartData = await cart.save();
+    cart = await cart.save();
+    const cartData = await cart.populate('items.product').execPopulate();
     return res.send(cartData);
   } catch (err) {
     next(err);
