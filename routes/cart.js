259f06de352ab4c502ef84d86d3249ccc7760a70
Basharath <basharath.in@gmail.com>
2021-08-25 20:57:54 +0530

    Adds cart checkout route

--- routes/cart.js
+++ routes/cart.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const auth = require('../middleware/auth');
 
 const { getCart, updateCart, clearCart } = require('../controllers/cart');
+const { checkoutCart } = require('../controllers/orders');
 
 router.get('/', auth, getCart);
 router.post('/', auth, updateCart);
+router.post('/checkout', auth, checkoutCart);
 router.delete('/', auth, clearCart);
 
 module.exports = router;
