82a93bf12567297887e19573b9f28ed759649b1d
Basharath <basharath.in@gmail.com>
2021-08-26 20:05:01 +0530

    Updates routes with params for single order requests

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
+const validateObjectId = require('../middleware/validateObjectId');
 
-const { getOrders, postOrder, cancelOrder } = require('../controllers/orders');
+const {
+  getOrders,
+  getOrder,
+  postOrder,
+  cancelOrder,
+} = require('../controllers/orders');
 
 router.get('/', auth, getOrders);
+router.get('/:id', validateObjectId, auth, getOrder);
 router.post('/', auth, postOrder);
-router.delete('/', auth, cancelOrder);
+router.delete('/:id', validateObjectId, auth, cancelOrder);
 
 module.exports = router;
