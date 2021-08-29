b168abe5d3e670a3a2e984e778fbb88545fba157
Basharath <basharath.in@gmail.com>
2021-08-29 12:48:38 +0530

    Adds change password route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { signIn, signUp } = require('../controllers/users');
+const { signIn, signUp, changePassword } = require('../controllers/users');
+const auth = require('../middleware/auth');
 
 router.post('/signin', signIn);
 router.post('/signup', signUp);
+router.post('/changepassword', auth, changePassword);
 
 module.exports = router;
